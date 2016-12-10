295eaebd0c59e815c4f4d78f0ef03c3aecd09b76
rgill02 <ryan.gill0308@gmail.com>
2016-12-10 01:17:18 -0500

    Pretty sure channel and both equalizers work

--- Code/ZeroForcingEqualizer.m
+++ Code/ZeroForcingEqualizer.m
@@ -1,4 +1,4 @@
-function [output] = ZeroForcingEqualizer(sample_width, pulse_time, h_coef, h, noise_power, signal, to_plot, is_hsp, K)
+function [output] = ZeroForcingEqualizer(fs, h_coef, signal)
 %ZeroForcingEqualizer Models the zero forcing equalizer associated with our
 %channel
 %   Zero Forcing Equalizer is used to undo the effects of the channel. It
@@ -8,56 +8,21 @@ function [output] = ZeroForcingEqualizer(sample_width, pulse_time, h_coef, h, no
 %   the signal after it has been passed through the equalizer
 %
 %   Input:
-%       sample_width = width of pulse in samples
-%       pulse_time = width of pulse in seconds
-%       h_coef = coefficients of channel filter (non zero padded)
-%       h = impulse response of channel (zero padded)
-%       noise_power = power of noise
+%       fs = sampling frequency
+%       h_coef = coefficients of the channel
 %       signal = vector representing signal to pass through equalizer
-%       to_plot = boolean if to show plots
-%       is_hsp = true if half sine pulse
-%       K = truncation parameter
 %   Outputs:
 %       output = vector representing equalized signal
 
-%% Plot Impulse and Frequency Response of Equalizer
-%plot frequency response of the zero forcing equalizer
-%freqz takes a numerator and denominator of the filter, z transform gives
-%the numerator of the channel which are just the pulse amplitudes,
-%therefore since the zero forcing filter is just the inverse then swap the
-%numerator and denominator
-if to_plot
-    figure;
-    freqz([1], h_coef);
-    title('Frequency Response of Zero Forcing Equalizer');
-end
-%call above plots is and call below stores frequency response
-[Q, w] = freqz([1], h_coef);
-
-%plot impulse response
+%upsample channel to get impulse response
+h = upsample(h_coef, fs);
+%compute frequency response
+H = fft(h, length(signal));
+%compute impulse response of ZF equalizer
+Q = 1 ./ H;
 q = ifft(Q);
-t = 0:1:length(q)-1;
-if to_plot
-    figure;
-    plot(t, real(q));
-    xlabel('Sample (n)');
-    ylabel('q(t)');
-    title('Impulse Response of Zero Forcing Equalizer');
-end
-
-%% Pass Signal Through Equalizer
-output = filter([1], h, signal);
-
-if to_plot
-    figure;
-    plot(output);
-    title('Signal After Zero Forcing Equalizer');
-end
-
-%% Create Eye Diagram
-if to_plot
-    RyanEyeDiagram(sample_width, pulse_time, noise_power, 'Eye Diagram After Zero Forcing Equalizer', output, is_hsp, K);
-end
+%apply ZF equalizer through convolution
+output = conv(q, signal);
 
 end
 
--- Code/test.m
+++ Code/test.m
@@ -3,55 +3,87 @@ close all;
 %define constants
 fs = 32;
 T = 1;
-is_hsp = false;
+is_hsp = true;
+is_zf = true;
 alpha = 0.5;
 K = 6;
-noise_power = 0;
-N = 1;
+noise_power = 0.1;
+N = 5;
+h_coef = [1 1/2 3/4 -2/7];
 
 %load image
 [Ztres, block_height, block_width, pixel_height, pixel_width, minval, maxval] = ImagePreProcess();
 bit_matrix = ConvertToBitstream(Ztres, N);
 
-%modulate signal
-if is_hsp
-    modulated_signal = Modulate_HSP(fs, T, bit_matrix(1,:));
-else
-    modulated_signal = Modulate_SRRC(fs, T, alpha, K, bit_matrix(1,:));
+received_bits = zeros(size(bit_matrix));
+for ii = 1:size(bit_matrix, 1)
+    
+    %modulate signal
+    if is_hsp
+        modulated_signal = Modulate_HSP(fs, T, bit_matrix(ii,:));
+    else
+        modulated_signal = Modulate_SRRC(fs, T, alpha, K, bit_matrix(ii,:));
+    end
+
+    %{
+    %plot modulated signal
+    figure;
+    plot(modulated_signal);
+    title('Modulated Signal');
+    %}
+
+    %send signal through channel
+    channel_output = Channel(fs, h_coef, modulated_signal);
+
+    %{
+    %plot output of channel
+    figure;
+    plot(channel_output);
+    title('Channel Output');
+    %}
+
+    %add noise to signal
+    noisy_signal = Noise(noise_power, channel_output);
+
+    %{
+    %plot noisy signal
+    figure;
+    plot(noisy_signal);
+    title('Signal with Noise');
+    %}
+
+    %pass signal through equalizer
+    if is_zf
+        equalizer_output = ZeroForcingEqualizer(fs, h_coef, noisy_signal);
+    else
+        equalizer_output = MMSEEqualizer(fs, h_coef, noise_power, noisy_signal);
+    end
+
+    %{
+    %plot output of equalizer
+    figure;
+    plot(equalizer_output);
+    title('Output of Equalizer');
+    %}
+
+    %pass signal through matched filter
+    matched_output = MatchedFilter(fs, T, alpha, K, equalizer_output, is_hsp);
+
+    %{
+    %plot output of matched filter
+    figure;
+    plot(matched_output);
+    title('Output of Matched Filter');
+    %}
+
+    %sample and detect signal
+    received_bits(ii,:) = SampleDetect(fs, T, K, matched_output, is_hsp, N);
+    
+    ii
 end
-
-%plot modulated signal
-figure;
-plot(modulated_signal);
-title('Modulated Signal');
-
-%add noise to signal
-noisy_signal = Noise(noise_power, modulated_signal);
-
-%plot noisy signal
-figure;
-plot(noisy_signal);
-title('Signal with Noise');
-
-%pass signal through matched filter
-matched_output = MatchedFilter(fs, T, alpha, K, noisy_signal, is_hsp);
-
-%plot output of matched filter
-figure;
-plot(matched_output);
-title('Output of Matched Filter');
-
-%sample and detect signal
-%TODO fix num of bits
-received_bits = SampleDetect(fs, T, K, matched_output, is_hsp, N);
 
 %restore image
-newZtres = ConvertFromBitstream(bit_matrix, N);
+newZtres = ConvertFromBitstream(received_bits, N);
 ImagePostProcess(newZtres, block_height, block_width, pixel_height, pixel_width, minval, maxval);
+title(['HSP=' num2str(is_hsp) ', ZF=' num2str(is_zf) ', Noise=' num2str(noise_power)]);
 
-%check if got correct bits
-if isequal(received_bits, bit_matrix(1,:)) == 0
-    ans = 'You done fucked up'
-else
-    ans = 'Nice job'
-end
